e2a9da307bf303c37c1b48ec64903abf06a32d61
Tom <chiatunglo@hotmail.com>
2022-06-13 23:07:28 +0000

    experiment with how the snake moves in the game by using setTimeout and setInterval to move the snake

--- client.js
+++ client.js
@@ -14,6 +14,12 @@ const connect = function () {
     conn.write('Name: TOM')
   });
 
+  conn.on('connect', () => {
+    setInterval(() => {
+      conn.write('Move: up')
+    }, 1000)
+  })
+
   conn.on('data', (data) => {
     console.log(data);
   });
